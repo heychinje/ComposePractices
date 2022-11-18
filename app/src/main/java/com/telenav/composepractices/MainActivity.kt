985ccc559dc5323cf11136855db4e500b1167c2f
Jason (Zheng, Haiqiang) <hqzheng@telenavsoftware.com>
2022-11-18 14:17:37 +0800

    Box layout

--- app/src/main/java/com/telenav/composepractices/MainActivity.kt
+++ app/src/main/java/com/telenav/composepractices/MainActivity.kt
@@ -3,71 +3,181 @@ package com.telenav.composepractices
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.telenav.composepractices.ui.theme.ComposePracticesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            /**
-             * There are 3 sample ways to arrange UI elements: Column, Row, and Box
-             *
-             * Column: place items vertically on the screen.
-             * arrangement: verticalArrangement
-             * alignment: horizontalAlignment
-             *
-             * Row: place items horizontally on the screen
-             * arrangement: horizontalArrangement
-             * alignment: verticalAlignment
-             *
-             */
-            Row(
-                modifier = Modifier
-                    .background(color = Color.LightGray)
-                    .fillMaxSize(0.5f),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Greeting("AB")
-                Greeting("CDEF")
-                Greeting("G")
-            }
+            BoxExample3()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(
-        text = name,
-        fontSize = 32.sp,
-        color = Color.Red,
-        textAlign = TextAlign.Center,
+fun BoxExample1() {
+    Box(
         modifier = Modifier
-            .background(color = Color.Yellow)
-            .border(2.dp, color = Color.Blue)
-            .padding(10.dp)
-            .border(2.dp, color = Color.Magenta)
-    )
-}
+            .background(color = Color.Green)
+            .size(180.dp, 300.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .size(125.dp, 100.dp)
+                .align(Alignment.TopEnd)
+        ) {
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposePracticesTheme {
-        Greeting("Android")
+        Text(
+            text = "Hi",
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier
+                .background(color = Color.White)
+                .size(90.dp, 50.dp)
+                .align(Alignment.BottomCenter)
+        )
     }
 }
+
+@Composable
+fun BoxExample2() {
+    Box(
+        modifier = Modifier
+            .background(color = Color.LightGray)
+            .fillMaxSize()
+    ) {
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.TopStart),
+            text = "TopStart"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.TopCenter),
+            text = "TopCenter"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.TopEnd),
+            text = "TopEnd"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.CenterStart),
+            text = "CenterStart"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.Center),
+            text = "Center"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.CenterEnd),
+            text = "CenterEnd"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.BottomStart),
+            text = "BottomStart"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.BottomCenter),
+            text = "BottomCenter"
+        )
+
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .padding(10.dp)
+                .align(Alignment.BottomEnd),
+            text = "BottomEnd"
+        )
+    }
+}
+
+@Composable
+fun BoxExample3() {
+    Box() {
+        Image(
+            painter = painterResource(id = R.drawable.code_screen),
+            contentDescription = "code screen"
+        )
+
+        Text(
+            text = "Code Screen",
+            style = MaterialTheme.typography.h4,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.textButtonColors(
+                backgroundColor = Color.White,
+                contentColor = Color.DarkGray
+            ),
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(10.dp)
+                .border(5.dp, Color.DarkGray, RectangleShape)
+        ) {
+            Text(
+                text = "Add To Cart"
+            )
+        }
+    }
+}
+
